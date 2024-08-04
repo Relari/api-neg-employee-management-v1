1cb3db2bfe72f372c7de5c14413972a1d1cc1061
Relari <renzolavador@gmail.com>
2024-08-04 03:50:53 -0500

    docs: Javadoc en la clase principal agregado

--- src/main/kotlin/com/pe/relari/api_neg_employee_management_v1/ApiNegEmployeeManagementV1Application.kt
+++ src/main/kotlin/com/pe/relari/api_neg_employee_management_v1/ApiNegEmployeeManagementV1Application.kt
@@ -3,6 +3,12 @@ package com.pe.relari.api_neg_employee_management_v1
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * Clase principal donde se va a iniciar el proyecto.
+ * @author Relari
+ * @since 1.0.0
+ */
+
 @SpringBootApplication
 class ApiNegEmployeeManagementV1Application
 
