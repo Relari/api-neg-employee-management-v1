7f970b6f09892b68ac4d871d9e97eb1355e9a0fa
Relari <renzolavador@gmail.com>
2024-08-05 00:11:39 -0500

    docs: Se agrega configuracion para la documentacion Swagger

--- src/main/kotlin/com/pe/relari/api_neg_employee_management_v1/controller/EmployeeController.kt
+++ src/main/kotlin/com/pe/relari/api_neg_employee_management_v1/controller/EmployeeController.kt
@@ -1,31 +1,92 @@
 package com.pe.relari.api_neg_employee_management_v1.controller
 
-import com.pe.relari.api_neg_employee_management_v1.util.REGEXP_ONLY_NUMBER
-
 import com.pe.relari.api_neg_employee_management_v1.model.api.EmployeeRequest
 import com.pe.relari.api_neg_employee_management_v1.model.api.EmployeeResponse
 import com.pe.relari.api_neg_employee_management_v1.model.domain.Employee
 import com.pe.relari.api_neg_employee_management_v1.service.EmployeeService
+import com.pe.relari.api_neg_employee_management_v1.util.REGEXP_ONLY_NUMBER
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.intellij.lang.annotations.Pattern
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
 
+@Tag(name = "Employee", description = "Employee Controller")
+@OpenAPIDefinition(
+    info = Info(
+        title = "Employee API",
+        version = "\${application.info.version}",
+        description = "\${application.info.description}"
+    )
+)
 @RestController
 @RequestMapping("\${application.api.path}")
 class EmployeeController(private val employeeService: EmployeeService) {
 
+    @Operation(
+        summary = "Listado de Empleados",
+        method = "GET",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Retorna todos los Empleados",
+                content = [
+                    Content(
+                        array = ArraySchema(
+                            schema = Schema(
+                                implementation = EmployeeResponse::class
+                            )
+                        ),
+                        mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+                ]
+            )
+        ]
+    )
     @GetMapping
-    fun findAll() = employeeService.findAll()
+    fun findAll(): List<EmployeeResponse> = employeeService.findAll()
             .map { EmployeeMapper.mapEmployeeResponse(it) }
 
+    @Operation(
+        summary = "Obtiene la informacion de un empleado.",
+        method = "GET",
+        parameters = [
+            Parameter(
+                description = "Id del Empleado",
+                name = "id",
+                `in` = ParameterIn.PATH,
+                example = "1",
+                required = true
+            )
+        ],
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Muestra al empleado por su ID.",
+                content = [
+                    Content(
+                        schema = Schema(implementation = EmployeeResponse::class),
+                        mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Error producido porque no encuentra al empleado.",
+                content = [Content()]
+            )
+        ]
+    )
     @GetMapping("/{id}")
     fun findByEmployeeId(
             @Pattern(value = REGEXP_ONLY_NUMBER)
--- src/main/kotlin/com/pe/relari/api_neg_employee_management_v1/model/api/EmployeeResponse.kt
+++ src/main/kotlin/com/pe/relari/api_neg_employee_management_v1/model/api/EmployeeResponse.kt
@@ -1,13 +1,70 @@
 package com.pe.relari.api_neg_employee_management_v1.model.api
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class EmployeeResponse(
      val idEmployee: Int,
+     @Schema(
+          description = "Representa el apellido paterno del empleado.",
+          name = "fatherLastName",
+          example = "Perez",
+          implementation = String::class
+     )
      val fatherLastName: String,
+
+     @Schema(
+          description = "Representa el apellido materno del empleado.",
+          name = "motherLastName",
+          example = "Mendoza",
+          implementation = String::class
+     )
      val motherLastName: String,
+
+     @Schema(
+          description = "Representa el nombre del empleado.",
+          name = "firstName",
+          example = "Ricardo",
+          implementation = String::class
+     )
      val firstName: String,
-     val position: String,
-     val sex: String,
+
+     @Schema(
+          description = "Representa el cargo o posición del empleado en la empresa.",
+          name = "jobTitle",
+          example = "DEVELOPER",
+          implementation = String::class
+     )
+     val jobTitle: String,
+
+     @Schema(
+          description = "Representa el género o sexo del empleado.",
+          name = "gender",
+          example = "M",
+          implementation = String::class
+     )
+     val gender: String,
+
+     @Schema(
+          description = "Representa el salario o sueldo del empleado.",
+          name = "salary",
+          example = "1500",
+          implementation = Double::class
+     )
      val salary: Double,
+
+     @Schema(
+          description = "Representa el estado del empleado.",
+          name = "isActive",
+          example = "true",
+          implementation = Boolean::class
+     )
      val isActive: Boolean,
+
+     @Schema(
+          description = "Representa la fecha de nacimiento del empleado.",
+          name = "birthdate",
+          example = "03-05-1995",
+          implementation = String::class
+     )
      val birthdate: String
 )
